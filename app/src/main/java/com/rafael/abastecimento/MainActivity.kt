c51019631fd6fc60b2df6071db18081e38bc43f7
rafanizio <43146133+rafanizio@users.noreply.github.com>
2019-03-26 21:44:00 -0300

    limpando o codigo

--- app/src/main/java/com/rafael/abastecimento/MainActivity.kt
+++ app/src/main/java/com/rafael/abastecimento/MainActivity.kt
@@ -3,8 +3,12 @@ package com.rafael.abastecimento
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.roundToInt
+import kotlin.math.roundToLong
 
 class MainActivity : AppCompatActivity() {
+
+    var msg = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,45 +16,37 @@ class MainActivity : AppCompatActivity() {
 
 
         btnCalcular.setOnClickListener {
+            if (edGasolina.text.toString().isEmpty() || edAlcool.text.toString().isEmpty()){
+                msg = "Algum campo esta vazio!"
+            }
             if (edGasolina.text.toString().isNotEmpty() || edAlcool.text.toString().isNotEmpty()){
-
                 compararPrecos()
             }
+
             else {
-                var msg = "Campos com valores vazios!"
-                txtResultados.text = msg
-
+                msg = "Os 2 campos estão vazios!"
             }
-
+            txtResultados.text = msg
         }
     }
 
     private fun compararPrecos(){
-        var comparacao = (edGasolina.text.toString().toFloat() * 0.70)
-        var alcool =  edAlcool.text.toString().toFloat()
+        val comparacao = (edGasolina.text.toString().toFloat() * 0.70)
 
-
+        val alcool =  edAlcool.text.toString().toFloat()
         if ((alcool > 0) && (comparacao > 0 )){
 
             if (alcool <= comparacao){
-
-                var msg = "Compre o Alcool!"
+                msg = "Compre o Alcool!"
                 txtResultados.text = msg
             }
-
             else {
-                var msg = "Compre a Gasolina"
-
+                msg = "Compre a Gasolina"
                 txtResultados.text = msg
-
             }
-
-
         }else {
             var msg = "Coloque números maiores que 0 para fazer a comparação"
-
             txtResultados.text = msg
-
         }
 
     }
